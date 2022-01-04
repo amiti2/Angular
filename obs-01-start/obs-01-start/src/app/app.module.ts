62bb1578a999386cb9dc5e7817b0d99d39df4549
amiti2 <arcirodha1@gmail.com>
2022-01-04 09:07:58 +0530

    understanding pipe, map and filter and rxjs operators

--- obs-01-start/obs-01-start/src/app/app.module.ts
+++ obs-01-start/obs-01-start/src/app/app.module.ts
@@ -5,16 +5,40 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { NopageComponent } from './nopage/nopage.component';
+const appRoutes=[
+
+  {
+    path:"home",
+    component:HomeComponent
+  },
+  {
+    path:"userfff/f",
+    component:UserComponent
+  },
+  
+  {
+    path:"nopage",
+    component:NopageComponent
+  },{
+    path:"**",
+    redirectTo:"nopage"
+  }
+  
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    UserComponent
+    UserComponent,
+    NopageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- obs-01-start/obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { interval, Subscription, observable, Observable } from 'rxjs'
+import { elementAt } from 'rxjs-compat/operator/elementAt';
+import { filter, map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -19,12 +22,34 @@ export class HomeComponent implements OnInit, OnDestroy {
     // )
  //new method
  let count: number=0;
- this.os= new Observable(observer=>{
+ const customObservable= new Observable(observer=>{
     let count = 0; 
    setInterval(()=>{      
      observer.next(count);
+     if(count == 2)
+     {
+       observer.complete();
+     }
+     if(count===3)
+     {
+       observer.error({name:'died'})
+     }
+
      count++;       
-   },1000); }).subscribe(data=>{ console.log('this is subscribed ' +  data +' times');});
+   },1000); })
+   
+
+   //customObservable.pipe(map(data=>{ return 'result:' + data}));
+   
+   this.os=customObservable.pipe(
+    filter((data:number)=>{
+          if ((data%2) ==0)
+          {
+            return true;
+          }
+          return false;
+    }), 
+    map(data=>{ return 'result:' + data})).subscribe(data=>{ console.log(data);},err=>{ console.log(err.name)},()=>{ console.log('completed')});
 }
   //   const customObservable= Observable.create(
   //         observer=>{

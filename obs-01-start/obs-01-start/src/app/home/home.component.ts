fe761fc3fa1a6956d53b97c6a0dba92225b6e2e8
amiti2 <arcirodha1@gmail.com>
2022-01-04 07:45:51 +0530

    correct new observable created. Previous one had some errors

--- obs-01-start/obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs'
+import { interval, Subscription, observable, Observable } from 'rxjs'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,10 +13,31 @@ export class HomeComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.os= interval(1000).subscribe(
-      (count)=>{ console.log(count);
-      }
-    )
-  }
+    // this.os= interval(1000).subscribe(
+    //   (count)=>{ console.log(count);
+    //   }
+    // )
+ //new method
+ let count: number=0;
+ this.os= new Observable(observer=>{
+    let count = 0; 
+   setInterval(()=>{      
+     observer.next(count);
+     count++;       
+   },1000); }).subscribe(data=>{ console.log('this is subscribed ' +  data +' times');});
+}
+  //   const customObservable= Observable.create(
+  //         observer=>{
+  //           let count =0;
+  //           setInterval(()=>{
+  //             observer.next(count);
+  //             count++;
+  //           },1000);
+  //         }
+ 
+  //   );
+  //  this.os= customObservable.subscribe(data=>{console.log(data)});
+
+  
 
 }

aec232d85b4037fd43e45a9602d9a51f9c2ac42a
amiti2 <arcirodha1@gmail.com>
2022-01-09 19:36:23 +0530

    forms : Template Driven approach complete

--- forms-td-start/forms-td-start/src/app/app.component.ts
+++ forms-td-start/forms-td-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { UseExistingWebDriver } from 'protractor/built/driverProviders';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +12,46 @@ export class AppComponent {
   defaultValue='teacher';
   defaultUserName="airodha";
   answer="";
+  genders:string[]=['male','female'];
+  isSubmitted:boolean =false;
+
+  user={
+    username:'',
+    email:'',
+    secret:'',
+    questionanswers:'',
+    gender:''
+  }
 
   suggestUserName() {
     const suggestedName = 'Superuser';
-  }
+    this.app.form.setValue({
+      userData :{
+        username:suggestedName,
+        email:"abc@yahoo.com"
+      },
+      secret:'pet',
+      questionanswers:'This is test dd',
+      gender:'female'
+    });
+
+//  this.app.form.patchValue({
+//       userData :{
+//         username:suggestedName
+//       }
+//     });
+   }
 
   onSubmit(f:NgForm)
   {
-    console.log(this.app)
+    console.log(this.app);
+    this.isSubmitted =true;
+    this.user.username =this.app.value.userData.username;
+    this.user.email=this.app.value.userData.email;
+    this.user.gender=this.app.value.gender;
+    this.user.questionanswers=this.app.value.questionanswers;
+    this.user.secret=this.app.value.secret;
+    this.app.form.reset();
+
   }
 }
